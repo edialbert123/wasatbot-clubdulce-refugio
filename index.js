e60e24e5b4c81ff3628f30db63a02d5965f70dbe
edialbert123 <edialbert123@gmail.com>
2026-09-24 14:17:10 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const path = require('path');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+        headless: true,
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
